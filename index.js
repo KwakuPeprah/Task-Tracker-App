791a7486b86322ff884f41ee13b82e8aefcc6ef0
Kwaku <kwakupeprah64@gmail.com>
2025-05-14 03:21:01 +0000

    2nd push

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(cors())
 app.use(helmet)
-
+app.use(cookieParser())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Database connected successfully....")
+}).catch((err) => {
+    console.log(err)
+})
+
 
 app.get("/", (req,res) =>{
     res.json({message: "Hello World"})
---
+++ models/usersModel.js
@@ -0,0 +1,45 @@
+const { required } = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: [true, 'Email is required!'],
+        trim: true,
+        unique: [true,"Email must be unique"],
+        minLength:[5, 'Email must be at least 5 characters long'],
+        lowercase: true,
+    },
+    password:{
+        type: String,
+        required: [true, "Password must be provided"],
+        trim: true,
+        select: false, // Do not return password by default
+
+    },
+    verified:{
+        type: Boolean,
+        default: false,
+    },
+    verificationCode:{
+        type: String,
+        select: false,
+    },
+    verificationCodeValidation:{
+        type: Number,
+        select: false,
+    },
+    forgotPasswordCode:{
+        type: String,
+        select: false,
+    },
+     forgotPasswordCodeValidation:{
+        type: Number,
+        select: false,
+    },
+},{
+    timestamps: true,// Automatically add createdAt and updatedAt fields
+});
+
+module.exports = mongoose.model('User', userSchema);
+// module.exports = mongoose.model('User', userSchema);
